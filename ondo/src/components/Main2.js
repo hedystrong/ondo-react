d10cbfacbde1f39b39586c67af1dcd71789640c6
Morning <morning@mstars-lab3-08s-iMac.local>
2023-01-18 12:35:37 +0800

    kk

--- ondo/src/components/Main2.js
+++ ondo/src/components/Main2.js
@@ -1,32 +1,50 @@
-export default function Main2({card}) {
-    return(
-        <div className="row col-8 pt-5 justify-content-between">   
-            {card.map((e) => {
-                return(
-                    <div className="d-flex pt-5 justify-content-between">
-                        <div>
-                            <div className="d-flex">
-                                <img src={e.createdUser.img} alt="a"/>
-                                <h2 id="name">{e.createdUser.Name}</h2>
-                            
+export default function Main2({ card, aside }) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className=" col-8 pt-5 justify-content-between ">
+                    {card.map((e) => {
+                        return (
+                            <div className="d-flex pt-5 justify-content-between">
+                                <div>
+                                    <div className="d-flex">
+                                        <img src={e.createdUser.img} alt="a" />
+                                        <h2 id="name">{e.createdUser.Name}</h2>
+
+                                    </div>
+                                    <div className="css">
+                                        <h2 id="title2">{e.head}</h2>
+                                        <p style={{ width: "250px" }}>{e.title}</p>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>{e.date}</span>
+                                        <span>{e.read}</span>
+                                        <span>{e.key}</span>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <img src={e.img} alt="a" />
+                                </div>
                             </div>
-                            <div>
-                                <h2 id="title2">{e.head}</h2>
-                                <p width="440px">{e.title}</p>
-                            </div>
-                            <div className="d-flex justify-content-between">
-                                <span>{e.date}</span>
-                                <span>{e.read}</span>
-                                <span>{e.key}</span>
-                            </div>
-                        </div>      
-                    
-                        <div>
-                            <img src={e.img} alt="a"/>
-                        </div>
-                    </div>
-                )
-            })}
+                        )
+                    })}
+                </div>
+                <div className="col-4">
+                    <h2 id="title3">Discover more of what matters to you</h2>
+                    {aside.map((e) => {
+                        return (
+                            <ul className="asideList d-flex flex-wrap">
+                                <li>
+                                    {e.title}
+                                </li>
+                            </ul>
+
+                        )
+                    })}
+                </div>
+            </div>
+
         </div>
     )
 }
--- ondo/src/components/Navbar.js
+++ ondo/src/components/Navbar.js
@@ -1,32 +1,43 @@
+import { Route, Routes } from "react-router-dom";
+import OurStory from "../components/OurStory";
+import Membership from "../components/Membership";
+import Write from "../components/Write";
 
+export default function Navbar({ logo, tses }) {
 
-export default function Navbar({logo,tses}){
-    
-    return(
+    return (
         <div className="container-fliud">
             <div className="navbar bg-warning">
                 <div>
-                    <img src= {logo} alt="#"/>
+                    <img src={logo} alt="#" />
                 </div>
                 <div className="menu d-flex">
                     {/* <div> */}
-                        <ul className="menu1">
-                            {
-                                tses.map((e) => {
-                                    return(
-                                        <li>{e.title}</li>
-                                    )
-                                })
-                            }
-                        </ul>
+                    <ul className="menu1">
+                        {
+                            tses.map((e) => {
+                                return (
+                                    <li>{e.title}</li>
+                                )
+                            })
+                        }
+                    </ul>
                     {/* </div> */}
                     <div>
-                        <button className="button1">Get started</button>     
-                    </div>       
-                    
+                        <button className="button1">Get started</button>
+                    </div>
+
                 </div>
-             </div>
+            </div>
+            <div>
+                <Routes>
+                    <Route exact path="/" element={<OurStory />}>
+                        <Route path="/membership" element={<Membership />} />
+                        <Route path="/write" element={<Write />} />
+                    </Route>
+                </Routes>
+            </div>
         </div>
-        
+
     )
 }
